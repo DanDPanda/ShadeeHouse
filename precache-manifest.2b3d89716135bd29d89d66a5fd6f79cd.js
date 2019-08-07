f216a5fcfc48a239fbf2955a99f4eb400118427e
DanDPanda <dan.v.dinh@gmail.com>
2019-08-06 20:18:28 -0500

    Updates

---
+++ precache-manifest.2b3d89716135bd29d89d66a5fd6f79cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10066c4b14523e5906644753b862540a",
+    "url": "/ShadeeHouse/index.html"
+  },
+  {
+    "revision": "49ef605a8dc60a937bed",
+    "url": "/ShadeeHouse/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ShadeeHouse/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "49ef605a8dc60a937bed",
+    "url": "/ShadeeHouse/static/js/main.c6c46a96.chunk.js"
+  },
+  {
+    "revision": "66a9947fafc9e6fda587",
+    "url": "/ShadeeHouse/static/js/runtime~main.ccbaab5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ShadeeHouse/static/media/logo.5d5d9eef.svg"
+  }
+]);
