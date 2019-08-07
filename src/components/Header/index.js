1d6eeb07262d010b9a087e25d5522ebea94f25f8
DanDPanda <dan.v.dinh@gmail.com>
2019-08-06 22:31:35 -0500

    Header up

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../../styles/header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar header-color">
+        <div className="logo">SHADEE HOUSE</div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
