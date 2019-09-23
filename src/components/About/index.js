53e6d13a459b073b2667607e8a2df2f0d2dc3ffb
DanDPanda <dan.v.dinh@gmail.com>
2019-09-22 20:53:18 -0500

    GOooood

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
 import "../../styles/about.css";
+import ImageGallery from "react-image-gallery";
 import earth from "../../images/Earth.png";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/"
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/"
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/"
+  }
+];
 
 class About extends Component {
   render() {
@@ -14,7 +30,15 @@ class About extends Component {
           otherwise curse to you because we only make the best! Hope to see you
           soon!
         </p>
-        <img className="about-photo" src={require("../../images/Earth.png")} />
+        <ImageGallery
+          items={images}
+          showThumbnails={false}
+          showNav={false}
+          showFullscreenButton={false}
+          showPlayButton={false}
+          autoPlay={true}
+          slideInterval={6000}
+        />
       </div>
     );
   }
